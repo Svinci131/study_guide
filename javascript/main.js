c530f3b953da99307bd4e5e72dd9afe0ce13bf5e
Svinci131 <svinci131@gmail.com>
2016-03-22 14:49:07 -0400

    create topic works

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,24 @@
 var myDataRef = new Firebase('https://study-guide.firebaseio.com/');
 var topics = myDataRef.child("topics"); 
 
+//set up a thing so select goes to default if new topic has something or vice-versa
+//or just thow error 
 $("#suggestion").on('submit', function (e) {
       e.preventDefault();
-      console.log ("works")	
+      //add new topic
+      var topic = $(".new-topic").val();
+      if (topic) {
+      	var newTopic = topics.child(""+topic);
+      	newTopic.set("no questions available")
+      }
+      
+      	
+      //update topic
+      //add questions
+
+
 });
+
+
 //
 //topics.push({question: "samantha"}, );

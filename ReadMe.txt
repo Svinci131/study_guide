c44c2b861c9272b0f4c0cc31f5131bb1d5398e2d
Svinci131 <svinci131@gmail.com>
2016-03-22 14:33:12 -0400

    added add page

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+//Basic site to cleanly store my study info 
+
+//Set up Fire Base
+	//Create Section
+	//Add Items to an existing Section
+//Set up login
--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
 /* CURRENTLY IN: javascript/main.js */
+var myDataRef = new Firebase('https://study-guide.firebaseio.com/');
+var topics = myDataRef.child("topics"); 
+
+$("#suggestion").on('submit', function (e) {
+      e.preventDefault();
+      console.log ("works")	
+});
+//
+//topics.push({question: "samantha"}, );

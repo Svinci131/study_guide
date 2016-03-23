66180e501c07930c096300f2572e171e41ff56e0
Svinci131 <svinci131@gmail.com>
2016-03-22 21:09:40 -0400

    added semantic themes working on toggle

--- javascript/learn.js
+++ javascript/learn.js
@@ -2,15 +2,27 @@ var React = require('react');
 var gen = require('./gen_functions')
 
 module.exports = React.createClass({
-	qAndA: function () {
+	toggle: function () {
+		var className = "caret down icon"
+		return className;
+		// body...
+	},
+	qAndA: function (className) {
+		
 		var obj = this.props.data[this.props.id];
-		console.log("here", obj)
+		
 		var questions = Object.keys(obj).map(function(el, i){
 			console.log(obj[el], i)
-			return (<div className="active title">
-		    <i className="dropdown icon"></i>
-				{obj[el].question}
-		  </div>)
+			return (
+				<div>
+					<div className="active title">
+					    <i className={className}></i>
+							{obj[el].question}
+					</div>
+					<div className="active content">
+				    	<p>{obj[el].answer}</p>
+				 	</div>
+				</div>)
 		});
 		return questions
 	},
@@ -19,7 +31,7 @@ module.exports = React.createClass({
 		return (<div>
 			<h2 className="ui dividing header">{gen._formatForSite(this.props.id)}</h2>
 			<div className="ui accordion">
-			{this.qAndA()}
+			{this.qAndA(this.toggle())}
 			</div>
 			</div>)
 	}
